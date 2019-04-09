d8bc80ebba82cb5f8510510e1309a84e54a27b9b
Hao <1084572961@qq.com>
2019-04-09 16:25:30 +0800

    BassDao

--- src/main/java/cn/guimei_mall/dao/BaseDao.java
+++ src/main/java/cn/guimei_mall/dao/BaseDao.java
@@ -1,4 +1,84 @@
 package cn.guimei_mall.dao;
 
 public class BaseDao {
+    private Connection conn ;
+    private PreparedStatement ps;
+    private ResultSet rs;
+    String driver = ConfigManager.getInstance().getValues("driver");
+    String url = ConfigManager.getInstance().getValues("url");
+    String username = ConfigManager.getInstance().getValues("username");
+    String password = ConfigManager.getInstance().getValues("password");
+    // 获取连接
+    public boolean getConn(){
+        boolean flag = false;
+        // 加载驱动
+        try {
+//            Context ctx = new InitialContext();
+//            DataSource ds = (DataSource) ctx.lookup("java:comp/env/jdbc/news");
+//            conn = ds.getConnection();
+            Class.forName(driver);
+            // 获取连接
+            conn = DriverManager.getConnection(url,username,password);
+            flag = true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return flag;
+    }
+
+    //查询
+    public ResultSet excuteSelect(String sql,Object[] p){
+        if (getConn()) {
+            //创建Statement 执行sql
+            try {
+                ps = conn.prepareStatement(sql);
+                for (int i = 0; i < p.length; i++) {
+                    ps.setObject(i + 1, p[i]);
+                }
+                rs = ps.executeQuery();
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return rs;
+    }
+
+    //更新
+    public int excutUpdateRows(String sql,Object[] p) {
+        int ire = 0;
+        if (getConn()) {
+            //创建Statement 执行sql
+            try {
+                ps = conn.prepareStatement(sql);
+                for (int i = 0; i < p.length; i++) {
+                    ps.setObject(i + 1, p[i]);
+                }
+                ire = ps.executeUpdate();
+                return ire;
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return ire;
+    }
+
+    // 关闭连接
+    public void closeConn(){
+        try {
+            if (rs != null){
+                rs.close();
+            }
+            if (ps != null){
+                ps.close();
+            }
+            if (conn != null){
+                conn.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
