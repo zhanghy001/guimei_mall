5f66f8b192fe6d8e98ae25d97c98be6b4e9c151f
Hao <1084572961@qq.com>
2019-04-09 16:46:28 +0800

    BassDao1

---
+++ src/main/java/cn/guimei_mall/util/ConfigManager.java
@@ -0,0 +1,47 @@
+package cn.guimei_mall.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * Created by Hao on 2019/3/20.
+ */
+public class ConfigManager {
+    //加载配置文件
+    // 饿汉模式
+//    static ConfigManager configManager = new ConfigManager();
+//    static {
+//        confingManager = new ConfigManager();
+//    }
+
+
+
+    static ConfigManager confingManager;
+    Properties properties;
+    public ConfigManager(){
+        properties = new Properties();
+        InputStream is = ConfigManager.class.getClassLoader().getResourceAsStream("database.properties");
+        try {
+            properties.load(is);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+
+        }
+    }
+
+    // 单利 懒汉模式
+    public static ConfigManager getInstance(){
+        if (confingManager == null){
+            confingManager = new ConfigManager();
+        }
+        return  confingManager;
+    }
+
+    // 根据key获得value方法
+    public String getValues(String key){
+        return properties.getProperty(key);
+    }
+
+}
